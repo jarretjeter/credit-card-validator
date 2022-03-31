9c5381beefd5463c2b3e9120e0adb368d61177fa
JeremyM45 <jeremyvs1945@gmail.com>
2022-03-31 11:41:27 -0700

    BUGFIX: fix problem with isNumber function only returning first element of array
    co-authored-by: Jarret Jeter <jarretjeter@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 function isNumber(number) {
   let array = number.split("");
-  array = parseInt(array)
-  let arrayOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  if(arrayOfNumbers.includes(array)) {
-    return 1
-  } else {
-    return 0
+  let numArray = []
+  array.forEach(function(e) {
+    e = parseInt(e)
+    numArray.push(e);
+  });
+  console.log(numArray)
+  let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (i = 0; i < numArray.length; i++) {
+    let num = numArray[i]
+    if (numbers.includes(num))  {
+      return "1"
+    } else {
+      return "0"
+    }
   }
 }
-
 function ignoreNonNumber(number) {
   let array = number.split("");
   let Numbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
@@ -21,11 +28,11 @@ function ignoreNonNumber(number) {
     return number
   }
 }
-
 function everyOtherNum(number) {
   let array = number.split("");
+  array = parseInt(array)
   console.log(array);
   for (let i = array.length -1; i > 0; i = i - 2) {
-    console.log(array[i])
+    array[i] = array[i] *2
   }
 }
